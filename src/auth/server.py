f112fec7d599037bb227ae8331b42fcc577e7ab3
Nilesh Pant <nileshpant6969@gmail.com>
2025-03-02 03:17:45 +0530

    feat: initial commit

---
+++ src/auth/server.py
@@ -0,0 +1,22 @@
+import jwt, datetime, os
+from flask import Flask
+from flask_mysqldb import MySQL
+
+# Create a Flask server
+server = Flask(__name__)
+mysql = MySQL(server)
+
+
+#configure the database
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+
+
